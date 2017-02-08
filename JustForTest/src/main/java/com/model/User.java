2f1b84ea6da48abf9402905ece2866e4cf4fa276
qwj <1592095287@qq.com>
2017-02-08 23:37:28 +0800

    alter User

--- JustForTest/src/main/java/com/model/User.java
+++ JustForTest/src/main/java/com/model/User.java
@@ -6,6 +6,7 @@ package com.model;
 public class User {
     private int id;
     private String username;
+    private int age;
 
     public int getId() {
         return id;
@@ -21,5 +22,13 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
