c8709975f15f766ed6177521bc102dad1290ecbc
qwj <1592095287@qq.com>
2017-02-08 23:34:50 +0800

    create User

---
+++ JustForTest/src/main/java/com/model/User.java
@@ -0,0 +1,25 @@
+package com.model;
+
+/**
+ * Created by qwj on 17/2/8.
+ */
+public class User {
+    private int id;
+    private String username;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
